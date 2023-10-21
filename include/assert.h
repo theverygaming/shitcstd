dcba5af6552f5b5f0531f3b9bba62897ee600bbb
theverygaming <clemibunge@gmail.com>
2023-10-21 07:23:11 +0200

    clang-format

--- include/assert.h
+++ include/assert.h
@@ -1,8 +1,13 @@
 #pragma once
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #ifndef NDEBUG
-#define assert(condition) do { if(!(condition)) { printf("assertion " #condition " failed - "); } } while(0)
+#define assert(condition)                                                                                                                                                                              \
+    do {                                                                                                                                                                                               \
+        if (!(condition)) {                                                                                                                                                                            \
+            printf("assertion " #condition " failed - ");                                                                                                                                              \
+        }                                                                                                                                                                                              \
+    } while (0)
 #else
-#define assert(ignore)((void) 0)
+#define assert(ignore) ((void)0)
 #endif
